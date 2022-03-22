adaf39599eb83c7a41800689b7d7fc46bd53524e
Nickolay Kondratyev <trase.nick@yandex.ru>
2022-03-22 22:39:28 +0300

    Initial commit

---
+++ src/main/java/ru/n1kk0n/App.java
@@ -0,0 +1,13 @@
+package ru.n1kk0n;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
